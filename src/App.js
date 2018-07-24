91f35ff70e2b66e2c31ddcd4c70be7787497da5f
Victor Velasquez <victorv24@gmail.com>
2018-07-24 13:34:06 -0700

    added npm stuff

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import contacts from './contacts.json'
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello world</h1>
+      <div className="container">
+        <h1>This is The Top Header</h1>
         <ul>
-        	<li>This is a test</li>
+        	<li>This is a test {contacts}</li>
         	<li>This is also a test</li>
         	<li>Hey, look at that. Another test</li>
         </ul>
