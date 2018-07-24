d612d8a9451ac8419e6dbc8a07bac8dc44b9a60f
Victor Velasquez <victorv24@gmail.com>
2018-07-24 13:25:43 -0700

    created contacts.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import contacts from './contacts.json'
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello world</h1>
+        <ul>
+        	<li>This is a test</li>
+        	<li>This is also a test</li>
+        	<li>Hey, look at that. Another test</li>
+        </ul>
       </div>
     )
   }
